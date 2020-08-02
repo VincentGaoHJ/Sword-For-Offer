5debbc90603ae65feeb6a857b8e752410df86dde
Haojun Gao <gaohaojun980707@sina.com>
2020-08-02 10:10:05 +0800

    Upload offer 25

---
+++ rsc/offer_25.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""
+@Time   : 2020/8/2  10:08 
+@Author : Haojun Gao (github.com/VincentGaoHJ)
+@Email  : vincentgaohj@gmail.com haojun.gao@u.nus.edu
+@Sketch : 剑指 Offer 25. 合并两个排序的链表
+"""
+
+from utils.Node import ListNode
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+
+def offer_25():
+    """
+    输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+    输入：1->2->4, 1->3->4
+    输出：1->1->2->3->4->4
+    限制：0 <= 链表长度 <= 1000
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    # 剑指 Offer 25. 合并两个排序的链表
+    offer_25()
