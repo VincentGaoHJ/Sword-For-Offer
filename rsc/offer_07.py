0a59cd4f74a66d98fff33a6de40cddbe2e041cb6
Haojun Gao <gaohaojun980707@sina.com>
2020-08-02 09:11:26 +0800

    Upload offer 07

--- rsc/offer_07.py
+++ rsc/offer_07.py
@@ -7,6 +7,8 @@
     https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof/
 """
 
+from utils.Node import TreeNode
+
 
 class Solution(object):
     def buildTree(self, preorder, inorder):
@@ -15,7 +17,17 @@ class Solution(object):
         :type inorder: List[int]
         :rtype: TreeNode
         """
-        pass
+        if len(inorder) == 0:
+            return None
+        # 获取当前节点
+        Node = TreeNode(preorder[0])
+        # 获取中序遍历分割点
+        idx = inorder.index(preorder[0])
+        # 递归开始左节点
+        Node.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])
+        # 递归开始右节点
+        Node.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])
+        return Node
 
 
 def offer_07():
@@ -42,6 +54,11 @@ def offer_07():
 
 # 根据子树特点，我们可以通过同样的方法对左（右）子树进行划分，每轮可确认三个节点的关系 。
 # 此递推性质让我们联想到用 递归方法 处理。
+# pre 3 -> inorder 9 | 15 20 7
+# pre 9
+# pre 20 -> inorder 15 | 7
+# pre 15
+# pre 7
 if __name__ == '__main__':
     # 剑指 Offer 07. 重建二叉树
     offer_07()
