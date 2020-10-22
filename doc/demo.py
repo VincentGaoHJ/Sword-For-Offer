0af92cc7fe941267a037c541d1e5f94deb095d66
Haojun Gao <gaohaojun980707@sina.com>
2020-10-22 10:24:21 +0800

    Upload Demo

---
+++ doc/demo.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+"""
+@Time   : 2020/10/22  10:23 
+@Author : Haojun Gao (github.com/VincentGaoHJ)
+@Email  : vincentgaohj@gmail.com haojun.gao@u.nus.edu
+@Sketch : 
+"""
+
+
+def offer_():
+    """"""
+    solution = Solution()
+    output = solution.maxSubArray()
+
+    print(output)
+
+
+if __name__ == '__main__':
+    offer_()
